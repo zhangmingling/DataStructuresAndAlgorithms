6c66dca02b16a3afa516b248a9f69b19a571394a
zhangmingling <mingling.zhang@outlook.com>
2024-10-14 17:38:47 +0800

    update the code

--- test/test.cpp
+++ test/test.cpp
@@ -1,80 +1,45 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <sstream>
-
 
 using namespace std;
 
-int minInstallTime(int N, vector<int>& time, vector<vector<int>>& dependencies) {
-    vector<vector<int>> graph(N);    // 邻接表，存储依赖关系
-    vector<int> inDegree(N, 0);      // 入度表
-    vector<int> dp(N, 0);            // dp数组，存储每个步骤的最早完成时间
-
-    // 构建图和入度表
-    for (int i = 0; i < N; i++) {
-        for (int dep : dependencies[i]) {
-            if (dep != -1) {
-                graph[dep - 1].push_back(i);
-                inDegree[i]++;
-            }
-        }
-    }
-
-    // 队列存放所有入度为0的步骤
-    queue<int> q;
-    for (int i = 0; i < N; i++) {
-        if (inDegree[i] == 0) {
-            q.push(i);
-            dp[i] = time[i];  // 没有依赖的步骤可以立即执行，完成时间就是其本身时间
-        }
-    }
-
-    // 拓扑排序
-    while (!q.empty()) {
-        int cur = q.front();
-        q.pop();
-
-        // 更新依赖于当前步骤的所有步骤
-        for (int next : graph[cur]) {
-            dp[next] = max(dp[next], dp[cur] + time[next]);  // 更新最早完成时间
-            inDegree[next]--;                               // 减少入度
-            if (inDegree[next] == 0) {
-                q.push(next);  // 当入度为0时，可以开始执行
-            }
-        }
-    }
-
-    // 返回所有步骤的最晚完成时间，即最小安装时间
-    return *max_element(dp.begin(), dp.end());
-}
-
 int main() {
-    int N;
-    cin >> N;
+    int num;
+    cout << "请输入一个四位数: ";
+    cin >> num;
 
-    vector<int> time(N);
-    for (int i = 0; i < N; i++) {
-        cin >> time[i];
+    // 确保输入是四位数
+    if (num < 1000 || num > 9999) {
+        cout << "请输入一个有效的四位数！" << endl;
+        return 1;
     }
 
-    vector<vector<int>> dependencies(N);
-    for (int i = 0; i < N; i++) {
-        string line;
-        getline(cin >> ws, line);
-        if (line == "-1") {
-            dependencies[i].push_back(-1);
-        } else {
-            stringstream ss(line);
-            int dep;
-            while (ss >> dep) {
-                dependencies[i].push_back(dep);
-            }
-        }
-    }
+    // 分别提取四位数字
+    int d1 = (num / 1000) % 10; // 第一位
+    int d2 = (num / 100) % 10;  // 第二位
+    int d3 = (num / 10) % 10;   // 第三位
+    int d4 = num % 10;          // 第四位
 
-    cout << minInstallTime(N, time, dependencies) << endl;
+    // 1. 每一位加5并取余10
+    d1 = (d1 + 5) % 10;
+    d2 = (d2 + 5) % 10;
+    d3 = (d3 + 5) % 10;
+    d4 = (d4 + 5) % 10;
+
+    // 2. 互换位置
+    // 第1位和第4位互换，第二位和第三位互换
+    int temp = d1;
+    d1 = d4;
+    d4 = temp;
+
+    temp = d2;
+    d2 = d3;
+    d3 = temp;
+
+    // 3. 合并为整数输出
+    int encrypted_num = d1 * 1000 + d2 * 100 + d3 * 10 + d4;
+
+    // 输出加密后的结果
+    cout << "加密后的结果是: " << encrypted_num << endl;
 
     return 0;
 }
