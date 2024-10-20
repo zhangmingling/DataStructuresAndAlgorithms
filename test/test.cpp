537ebf4f52f934e3b6559e7a638db5c5ba4d6dae
zhangmingling <mingling.zhang@outlook.com>
2024-10-20 15:39:37 +0800

    uodate the code

--- test/test.cpp
+++ test/test.cpp
@@ -1,45 +1,18 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    int num;
-    cout << "请输入一个四位数: ";
-    cin >> num;
-
-    // 确保输入是四位数
-    if (num < 1000 || num > 9999) {
-        cout << "请输入一个有效的四位数！" << endl;
-        return 1;
+#include <stdio.h>
+int main()
+{
+    int y = 1;
+    while (y < 5)
+    {
+        if (y % 2 == 0)
+        {
+            printf("%d is even\n", y);
+        }
+        else
+        {
+            printf("%d is odd\n", y);
+        }
+        y++;
     }
-
-    // 分别提取四位数字
-    int d1 = (num / 1000) % 10; // 第一位
-    int d2 = (num / 100) % 10;  // 第二位
-    int d3 = (num / 10) % 10;   // 第三位
-    int d4 = num % 10;          // 第四位
-
-    // 1. 每一位加5并取余10
-    d1 = (d1 + 5) % 10;
-    d2 = (d2 + 5) % 10;
-    d3 = (d3 + 5) % 10;
-    d4 = (d4 + 5) % 10;
-
-    // 2. 互换位置
-    // 第1位和第4位互换，第二位和第三位互换
-    int temp = d1;
-    d1 = d4;
-    d4 = temp;
-
-    temp = d2;
-    d2 = d3;
-    d3 = temp;
-
-    // 3. 合并为整数输出
-    int encrypted_num = d1 * 1000 + d2 * 100 + d3 * 10 + d4;
-
-    // 输出加密后的结果
-    cout << "加密后的结果是: " << encrypted_num << endl;
-
     return 0;
 }
